eff7d3f6647f555f5457ac8fb8be7f12fe94e071
fbastian <frederic.bastian@gmail.com>
2020-03-26 15:57:07 +0100

    Correct constraints and indexes on OMAHierarchicalGroup in db_creation, otherwise the FK creation for geneToOma fails

--- pipeline/db_creation/bgeeIndex.sql
+++ pipeline/db_creation/bgeeIndex.sql
@@ -1,6 +1,10 @@
 -- this file contains the indexes that do not add any constraints, defined solely
--- for performance issues (unique indexes are therefore not present in this file,
--- but in bgeeConstraint.sql)
+-- for performance issues or for FK definitions from other tables
+-- (unique indexes are therefore not present in this file, but in bgeeConstraint.sql)
+
+-- index needed for FK constraints from table geneToOma.
+-- TODO: geneToOma needs to be updated and is currently not filled, to reevaluate
+ALTER TABLE OMAHierarchicalGroup ADD INDEX (OMAGroupId);
 
 -- index needed to improve performances when inserting ranks in affymetrixProbeset table
 ALTER TABLE affymetrixProbeset ADD INDEX (bgeeGeneId, bgeeAffymetrixChipId);
