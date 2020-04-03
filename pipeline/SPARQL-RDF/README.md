644af8cf144cf16f04f8ae1ca4178c7ebeace712
tmendes <tarcisio.mendes@sib.swiss>
2020-04-03 10:50:46 -0300

    added bash script to create RDF data

--- pipeline/SPARQL-RDF/README.md
+++ pipeline/SPARQL-RDF/README.md
@@ -1,11 +1,39 @@
 **Requirements**: having successfully run dblite_creation
+##Prerequirements 
+[Install Virtuoso 7.2](http://vos.openlinksw.com/owiki/wiki/VOS#How%20Do%20I%20Install%20Virtuoso%3F)
+Edit [virtuoso.ini](http://docs.openlinksw.com/virtuoso/dbadm/) configuration file to allow for reading/writing files in the directory that contains the RDF-based files.
+
+virtuoso.ini line to edit: DirsAllowed = ., ../vad, /new/path/bgee/ttl
+
+Download and extract [Ontop-cli 3.0.1](https://sourceforge.net/projects/ontop4obda/files/ontop-3.0.1/ontop-cli-3.0.1.zip/download) command line tool. 
 
 **Goals**:
 * Set up the Bgee SPARQL endpoint
 
-## Details
 
-Set up the Bgee SPARQL endpoint with bgeelite_v*RELEASE* database and mapping files, without tool such as Virtuoso.
+
+## Running the easyBgee to RDF converter
+
+Set up the Bgee SPARQL endpoint with easyBgee_v*RELEASE* database and mapping files for Ontop tool.
+
+To run the bgee_rdf.sh bash script:
+ easyBgee to RDF triples and load into a Virtuoso data store
+                [ {-x | --ontop} <ontop directory> ]
+                [ {-m | --mapping-file} <mapping file> ]
+                [ {-o | --output-dir-path} <Turtle file output> ]
+                [ {-p | --ontop-property-file} <property file>  ]
+                [ {-t | --ontology-file} <ontology file> ]
+                [ {-i | --isql} <iSQL tool from Virtuoso> ]
+                [ {-s | --host-port} <host and port to connect to Virtuoso, e.g. locahost:1111> ]
+                [ {-w | --virtuoso-pwd} <Virtuoso password> ]
+                [ {-u | --virtuoso-user} <Virtuoso username> ]
+                [ {-v | --bgee-version} <the bgee version, e.g. '14_1'> ]
+                [ {-h | --help} <usage help> ]
+
+   Example:              
+    ./bgee_rdf.sh -m ./conf/genex_adapt.obda -o /new/path/bgee/ttl -p  ./conf/genex_adapt.properties -x ./ontop-cli-3 -t ./conf/genex_adapt.owl -i ~/not_save/virtuoso/bin/isql -s 1111 -w dba -u dba -v '14_1'
+
+
 
 ## Data generation
 
---
+++ pipeline/SPARQL-RDF/bgee_rdf.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+usage()
+{
+    echo "usage: easyBgee to RDF triples and load into a Virtuoso data store
+                [ {-x | --ontop} <ontop directory> ]
+                [ {-m | --mapping-file} <mapping file> ]
+                [ {-o | --output-dir-path} <Turtle file output> ]
+                [ {-p | --ontop-property-file} <property file>  ]
+                [ {-t | --ontology-file} <ontology file> ]
+                [ {-i | --isql} <iSQL tool from Virtuoso> ]
+                [ {-s | --host-port} <host and port to connect to Virtuoso, e.g. locahost:1111> ]
+                [ {-w | --virtuoso-pwd} <Virtuoso password> ]
+                [ {-u | --virtuoso-user} <Virtuoso username> ]
+                [ {-v | --bgee-version} <the bgee version, e.g. '14_1'> ]
+                [ {-h | --help} <usage help> ]
+
+   Example:              
+    ./bgee_rdf.sh -m ./conf/genex_adapt.obda -o ./ttl -p  ./conf/genex_adapt.properties -x ./ontop-cli-3 -t ./conf/genex_adapt.owl -i ~/not_save/virtuoso/bin/isql -s 1111 -w dba -u dba -v '14_1'
+    "
+
+}
+#Variables to be assigned, currently the default is no value.
+ontop_dir_path=
+obda_file_path=
+output_dir_path=
+ontop_property_file=
+ontology_file=
+isql_file_path=
+virtuoso_host=
+virtuoso_user=
+virtuoso_pwd=
+bgee_version=
+
+if [ "$1" == "" ]
+then usage
+     exit 1
+fi
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -x | --ontop )          shift
+                                ontop_dir_path=${1%/}
+                                ;;
+        -m | --mapping-file )   shift
+                                obda_file_path=${1%/}
+                                ;;
+        -o | --output-dir-path ) shift
+                                 output_dir_path=${1%/}
+                                ;;
+        -p | --ontop-property-file ) shift
+                                     ontop_property_file=${1%/}
+                                    ;;  
+        -t | --ontology-file )  shift
+                                ontology_file=${1%/}
+                                ;;          
+        -i | --isql )            shift
+                                isql_file_path=${1%/}
+                                ;;
+        -s | --host-port )       shift
+                                virtuoso_host=${1%/}
+                                ;;      
+        -w | --virtuoso-pwd )  shift
+                                virtuoso_pwd=${1%/}
+                                ;;
+        -u | --virtuoso-user )   shift
+                                virtuoso_user=${1%/}
+                                ;;
+        -v | --bgee-version )   shift
+                                bgee_version=${1%/}
+                                ;;                                                                                                       
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
+echo "Starting RDF creation and loading into Virtuoso data store..."
+echo "Creating RDF data and serializing them as TURTLE files..."
+$ontop_dir_path/ontop materialize --separate-files -m $obda_file_path -f turtle -o $output_dir_path -p $ontop_property_file -t $ontology_file  
+#--disable-reasoning 
+
+# for MacOS it has the empty string argument ''
+#sed -i '' 's," \.,"^^xsd:double.,g' $output_dir_path/http___purl_org_genex_hasExpressionLevelP_*.ttl 
+sed -i 's," \.,"^^xsd:double.,g' $output_dir_path/http___purl_org_genex_hasExpressionLevelP_*.ttl 
+
+echo "The RDF data were created and saved as TURTLE files..."
+
+##### README #######
+#To place and execute at the directory that cotains the TTL files. Afterwards, in rbioinfo
+#/usr/local/virtuoso-opensource/bin/isql enviroment, run the command LOAD Path/To/Generated/add_bgee_virtuoso.sql
+####################
+
+echo "Generating iSQL script to load RDF data into a virtuoso data store..."
+dir_temp= `pwd`
+
+cd $output_dir_path/
+ls *.ttl > "./ttl_files.txt"
+input="./ttl_files.txt"
+output="./add_bgee_virtuoso.sql"
+
+while IFS= read -r line
+do
+  file=`pwd`"/$line"   
+  echo "DB.DBA.TTLP_MT(file_to_string_output('$file'), '', 'https://bgee.org/rdf_v$bgee_version');" >>  "$output"
+done < "$input"
+echo "SPARQL LOAD <http://purl.org/genex#> INTO <https://bgee.org/rdf_v$bgee_version>;" >>  "$output"
+rm -f ./ttl_files.txt
+cd dir_temp
+
+echo "Executing iSQL script to load RDF data into a virtuoso data store..."
+$isql_file_path  $virtuoso_host $virtuoso_user $virtuoso_pwd "EXEC=LOAD '$output_dir_path'/add_bgee_virtuoso.sql"
+
+echo "Finished. All files were loaded into a virtuoso data store."
