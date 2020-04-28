52658ace3c1bf56ad0870c6faeacfd727ab6b81f
Sébastien Moretti <smoretti@unil.ch>
2020-04-28 18:43:01 +0200

    Proper column parsing for MAS5

--- pipeline/Affymetrix/affy_utils.pl
+++ pipeline/Affymetrix/affy_utils.pl
@@ -115,17 +115,19 @@ sub extractProbesetsFromFile {
                 chomp $line;
                 $nbr_line++;
                 my @tmp = split(/\t/, $line);
-                my $call       = bgeeTrim($tmp[$column_indexes{'call'}]);
-                my $signal     = bgeeTrim($tmp[$column_indexes{'signal'}]);
-                my $probesetId = bgeeTrim($tmp[$column_indexes{'probeset_id'}]);
+                my $call          = bgeeTrim($tmp[$column_indexes{'call'}]);
+                my $signal        = bgeeTrim($tmp[$column_indexes{'signal'}]);
+                my $probesetId    = bgeeTrim($tmp[$column_indexes{'probeset_id'}]);
+                my $adjusted_call = bgeeTrim($tmp[$column_indexes{'adjusted_call'}]);
+                my $q_value       = bgeeTrim($tmp[$column_indexes{'q_value'}]);
 
                 # check that the call  and the signal intensity are defined
                 if ( exists $correspCall{$call} && defined $signal && $signal ne 'null' ) {
                     #pbset -> quality, call and signal
                     $pbsets{$probesetId}->{'call'}          = $correspCall{$call};
-                    $pbsets{$probesetId}->{'p_value'}       = 1.0;
-                    $pbsets{$probesetId}->{'q_value'}       = 1.0;
-                    $pbsets{$probesetId}->{'adjusted_call'} = $correspCall{$call};
+                    $pbsets{$probesetId}->{'p_value'}       = 1.0; # Because no p-value for MAS5
+                    $pbsets{$probesetId}->{'q_value'}       = $q_value;
+                    $pbsets{$probesetId}->{'adjusted_call'} = $adjusted_call;
                     # a way to convert signal to numeric
                     if ( $signal + 0 >= 0 ) {
                         $pbsets{$probesetId}->{'signal'} = $signal;
