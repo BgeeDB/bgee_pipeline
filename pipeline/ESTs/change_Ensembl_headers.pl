9ae1ba4eb0de7ffdbe1a4b67e6039c63a101afa6
Sébastien Morett <smoretti@unil.ch>
2021-08-02 14:06:09 +0200

    makeblastdb extra parameter name discarded

---
+++ pipeline/ESTs/change_Ensembl_headers.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl
+
+# Perl embedded modules
+use strict;
+use warnings;
+use diagnostics;
+
+use File::Slurp;
+
+if ( $#ARGV ne 0 ){
+    print "\n\tUSAGE: $0 Homo_sapiens.GRCh38.cdna.all.fa\n\n"; exit 1;
+}
+
+# Read file in list context and directly chomp lines
+for my $line ( read_file($ARGV[0], chomp => 1) ){
+    if ( $line =~ m/^>.+? gene:([^ ]+?)(\.\d+)? / ){
+        print ">$1\n";
+    }
+    else {
+        print "$line\n";
+    }
+}
+
+exit 0;
+
