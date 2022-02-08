f7efe410637f5b8bfd206d8c8d5b0b8c23052cd0
Julien <julien.wollbrett@gmail.com>
2022-02-08 16:00:38 +0100

    updated GenBank and RefSeq datasources

--- pipeline/db_creation/insert_data_sources.sql
+++ pipeline/db_creation/insert_data_sources.sql
@@ -77,8 +77,8 @@ INSERT INTO dataSource (dataSourceId, dataSourceName, XRefUrl, experimentUrl, ev
 (35, 'GSA', '', 'http://bigd.big.ac.cn/search?dbId=gsa&q=[experiment_id]', 'http://bigd.big.ac.cn/gsa/browse/[experiment_id]/[evidence_id]',
      'http://bigd.big.ac.cn/gsa/', 'RNA-Seq data source for various species', 1, 'RNA-Seq data source'),
 (36, 'OncoMX', 'https://oncomx.org/searchview/?gene=[xref_id]', '', '', 'https://www.oncomx.org', 'Integrated cancer mutation and expression resource', 0, ''),
-(37, 'RefSeq', 'https://www.ncbi.nlm.nih.gov/assembly/[assembly_id]/', '', '', 'https://www.ncbi.nlm.nih.gov/refseq/', 'Source for genome assembly and annotations', 1, 'Genomics database'),
-(38, 'GenBank', 'https://www.ncbi.nlm.nih.gov/assembly/[assembly_id]/', '', '', 'https://www.ncbi.nlm.nih.gov/genbank/', 'Source for genome assembly and annotations', 1, 'Genomics database'),
+(37, 'RefSeq', 'https://www.ncbi.nlm.nih.gov/nuccore/[xref_id]/', '', '', 'https://www.ncbi.nlm.nih.gov/refseq/', 'Source for genome assembly and annotations', 1, 'Genomics database'),
+(38, 'GenBank', 'https://www.ncbi.nlm.nih.gov/nuccore/[xref_id]/', '', '', 'https://www.ncbi.nlm.nih.gov/genbank/', 'Source for genome assembly and annotations', 0, 'Genomics database'),
 (39, 'Amphiencode', '', '', '', 'https://amphiencode.github.io/Data/', 'Source for the European amphioxus annotations', 1, 'Genomics database'),
 (40, 'GeneCards', 'https://www.genecards.org/cgi-bin/carddisp.pl?gene=[xref_id]', '', '', 'https://www.genecards.org/', 'GeneCards is a searchable, integrative database that provides comprehensive, user-friendly information on all annotated and predicted human genes.', 0, '');
 
