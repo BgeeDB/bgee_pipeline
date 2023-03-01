711dba84bf2ba0d87f161009382300f5da56ffc8
jwollbrett <julien.wollbrett@sib.swiss>
2023-03-01 16:38:04 +0100

    run fastp on target base libraries and better manage FASTQ file names resolution

--- pipeline/scRNA_Seq/Droplet_based_Protocols/0Preparation/download_SRA.R
+++ pipeline/scRNA_Seq/Droplet_based_Protocols/0Preparation/download_SRA.R
@@ -16,6 +16,10 @@
 library(dplyr)
 library(HelpersMG)
 library(tools)
+if (!requireNamespace("Rfastp", quietly = TRUE)) {
+  BiocManager::install("Rfastp")
+}
+library(Rfastp)
 
 ## reading arguments
 cmd_args = commandArgs(TRUE);
@@ -36,7 +40,7 @@ for( c_arg in command_arg ){
 
 ## Read EBI file. If file not exists, script stops
 if( file.exists(metadata_info) ){
-  readFile <- read.table(metadata_info, h=T, sep="\t")
+  library_metadata <- read.table(metadata_info, h=T, sep="\t")
 } else {
   stop( paste("metadata file not found [", metadata_info, "]\n"))
 }
@@ -47,15 +51,12 @@ if( file.exists(librariesDownloadedJura) ){
 } else {
   stop( paste("Libraries from JURA file not found [", librariesDownloadedJura, "]\n"))
 }
-
 ## Check libraries
-checkId <- data.frame(readFile$library_id[!(readFile$library_id %in% libDownloadJura$library_id)])
+checkId <- data.frame(library_metadata$library_id[!(library_metadata$library_id %in% libDownloadJura$library_id)])
 colnames(checkId) <- "library_id"
-finalLibsToDown <- dplyr::filter(readFile, library_id %in% checkId$library_id)
-
+finalLibsToDown <- dplyr::filter(library_metadata, library_id %in% checkId$library_id)
 ## remove NA and fastq.gz information that belongs to HCA (download with other script) or from EBI (download .bam files)
-finalLibsToDown <- dplyr::filter(readFile, fastq_ftp != "NA" & fastq_ftp != "fastq.gz")
-
+finalLibsToDown <- dplyr::filter(library_metadata, source = "SRA")
 
 ####################### FUNCTIONS ###############################
 check_seq_length <- function(fileName) {
@@ -64,14 +65,17 @@ check_seq_length <- function(fileName) {
   close(connection)
   seq_length <- strsplit(x = regmatches(x = first_line, m = regexpr("length=[0-9]+$",first_line)),
     split = "=")[[1]][2]
-  return(seq_length)
+  return(as.integer(seq_length))
 }
 #################################################################
 
 
 folder_experiments <- file.path(output, "EXPERIMENTS")
 for (library in  unique(finalLibsToDown$library_id)) {
-
+  print(library)
+  # list all R1 and R2 files to run fastp QC
+  r1_files <- character(0)
+  r2_files <- character(0)
   ## create output for each library
   message("treating the library: ", library)
   species <- unique(finalLibsToDown$tax_id[finalLibsToDown$library_id == library])
@@ -130,12 +134,24 @@ for (library in  unique(finalLibsToDown$library_id)) {
       }
     }
     ## check that size sequences from I1 < R1 < R2
-    if (is.unsorted(name_to_seq_length[,2]) ||
-        name_to_seq_length[1,2] > name_to_seq_length[2,2]) {
+    if (is.unsorted(as.integer(name_to_seq_length[,2])) ||
+        as.integer(name_to_seq_length[1,2]) > as.integer(name_to_seq_length[2,2])) {
       stop("the sequence size in SRA files does not match the assumption that",
         " file _1.fastq has sequence smaller than _2.fastq file which itself",
-        " has smaller sequences than _3.fastq (if it exists). It is then not",
-        " possible to rename SRA files for run ", run)
+        " has smaller sequences than _3.fastq (if it exists). But was ", name_to_seq_length,
+        " We make the assumption that files are ok but not named properly for run ", run, 
+	". We reordered the files and Sequence length are now :")
+      name_to_seq_length <- name_to_seq_length[order(as.integer(name_to_seq_length[,2])),]
+      if (length(fastq_files_names) == 3) {
+        warning("\tSequence size in I1 : ", name_to_seq_length[1,2],
+	     "\n\tSequence size in R1 : ", name_to_seq_length[2,2],
+	     "\n\tSequence size in R2 : ", name_to_seq_length[3,2])
+      } else if (length(fastq_files_names) == 2) {
+        warning("\tSequence size in R1 : ", name_to_seq_length[1,2],
+             "\n\tSequence size in R2 : ", name_to_seq_length[2,2])
+      } else {
+        stop("wrong number of files")
+      }
     }
     print(name_to_seq_length)
     ## if 3 files we consider that there are I1, R1 and R2
@@ -148,19 +164,21 @@ for (library in  unique(finalLibsToDown$library_id)) {
       old_file_name <- basename(name_to_seq_length[row_number,1])
       if (with_I1_file) {
         if (row_number == 1) {
-	  new_file_name <- gsub("_1", "_I1", old_file_name)
+	        new_file_name <- gsub("_1", "_I1", old_file_name)
         } else if (row_number == 2) {
-	  new_file_name <- gsub("_2", "_R1", old_file_name);
+	        new_file_name <- gsub("_2", "_R1", old_file_name);
+          r1_files <- c(r1_files, new_file_name)
         } else if (row_number == 3) {
           new_file_name <- gsub("_3", "_R2", old_file_name);
+          r2_files <- c(r2_files, new_file_name)
 	}
       } else {
         if (row_number == 1) {
           new_file_name <- gsub("_1", "_R1", old_file_name);
-          print(paste0("row 1 rename ", old_file_name, " to ", new_file_name))
+          r1_files <- c(r1_files, new_file_name)
         } else if (row_number == 2) {
-	  new_file_name <- gsub("_2", "_R2", old_file_name);
-          print(paste0("row 2 rename ", old_file_name, " to ", new_file_name))
+	        new_file_name <- gsub("_2", "_R2", old_file_name);
+          r2_files <- c(r2_files, new_file_name)
         }
       }
       file.rename(from = file.path(fastq_dir_path, old_file_name), to = file.path(fastq_dir_path, new_file_name))
@@ -169,8 +187,11 @@ for (library in  unique(finalLibsToDown$library_id)) {
     # everything was done properly for this run. It is now possible to delete the .sra file
     file.remove(sra_file)
   }
-
+  # run fastp QC on the downloaded library
+  umi_json_report <- rfastp(read1 = r1_files, read2 = r2_files, 
+    outputFastq = file.path(folder_library, 'fastp_qc'), umi = TRUE, umiLoc = "read1",
+    umiLength = 16)
   ## create a symlink following pattern */EXPERIMENTS/experiment_ID/library_id/
-  file.symlink(from = folder_library, to = file.path(folder_experiments, experiment, library), overwrite = FALSE, recursive = TRUE)
+  #file.symlink(from = folder_library, to = file.path(folder_experiments, experiment, library))
 }
 
