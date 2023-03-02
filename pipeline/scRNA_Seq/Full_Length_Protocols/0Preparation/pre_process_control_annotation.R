06bc8e863fd27dbe3dd09584e39d060e6703e00f
Julien <julien.wollbrett@gmail.com>
2023-03-02 15:04:34 +0100

    solve bug filtering too many libraries

--- pipeline/scRNA_Seq/Full_Length_Protocols/0Preparation/pre_process_control_annotation.R
+++ pipeline/scRNA_Seq/Full_Length_Protocols/0Preparation/pre_process_control_annotation.R
@@ -10,11 +10,7 @@
 ## output_folder --> folder where the generated file should be saved
 
 ## Libraries
-library(tools)
-library(readr)
-library(stringr)
 library(dplyr)
-library(forcats)
 
 ## reading arguments
 cmd_args = commandArgs(TRUE);
@@ -34,70 +30,42 @@ for( c_arg in command_arg ){
 }
 
 ## Read scRNASeqLibrary file, if file not exists, script stops
-if( file.exists(scRNASeqLibrary) ){
-  annotation <- read.table(scRNASeqLibrary, h=T, sep="\t", comment.char="")
-  names(annotation)[1] <- "libraryId"
-  ## just replace NA in sex and strain per "missing information" to be a level in factor (levels())
-  annotation$sex <- forcats::fct_explicit_na(annotation$sex)
-  annotation$strain <- forcats::fct_explicit_na(annotation$strain)
-} else {
-  stop( paste("scRNASeqLibrary file not found [", scRNASeqLibrary, "]\n"))
+if (!file.exists(scRNASeqLibrary)) {
+  stop( paste("scRNASeqLibrary file not found [", scRNASeqLibrary, "]"))
 }
+annotation <- read.table(scRNASeqLibrary, h=T, sep="\t", comment.char="")
+names(annotation)[1] <- "libraryId"
+## replace all empty values per NA to avoid having both NA and empty string in a condition parameter
+annotation[annotation == ''] <- NA
 
 #################################################################################
 ## create new output files
 pass_Libraries <- file.path(output_folder, "passScRNASeqLibrary.tsv")
-if (file.exists(pass_Libraries)){
-  message("File already exists and will be removed to create a new one to avoid overwritting!")
-  file.remove(pass_Libraries)
-  file.create(pass_Libraries)
-} else {
-  file.create(pass_Libraries)
-}
+file.create(pass_Libraries)
+
 notpass_Libraries <- file.path(output_folder,"notPassScRNASeqLibrary.tsv")
-if (file.exists(notpass_Libraries)){
-  message("File already exists and will be removed to create a new one to avoid overwritting!")
-  file.remove(notpass_Libraries)
-  file.create(notpass_Libraries)
-} else {
-  file.create(notpass_Libraries)
-}
+file.create(notpass_Libraries)
 
-#TODO: maybe collect all pass/not_pass info in a DF and write the 2 files after (directly with the proper colnames)
-for (species in unique(annotation$speciesId)) {
-  message("Species:", species)
-  for (experiment in unique(annotation$experimentId[annotation$speciesId == species])){
-    message("Name of experiments that belongs to the species:", experiment)
-    for (cellId in unique(annotation$cellTypeId_abInitio[annotation$experimentId == experiment])){
-      message("CellId info:", cellId)
-      for (stageId in unique(annotation$stageId[annotation$cellTypeId_abInitio == cellId])){
-        message("StageId info:", stageId)
-        for (strain in unique(annotation$strain[annotation$stageId == stageId])){
-          message("Strain info:", strain)
-          for (uberonId in unique(annotation$anatId[annotation$strain == strain])){
-            message("UberonId info:", uberonId)
-            for (sex in unique(annotation$sex[annotation$anatId == uberonId])){
-              message("sex info:", sex)
+#create a new empty data.frame for libs that passed the threshold
+passed <- data.frame(matrix(nrow = 0, ncol = ncol(annotation)))
+colnames(passed) <- colnames(annotation)
 
-              infoLib <- data.frame(annotation$libraryId[annotation$speciesId == species & annotation$experimentId == experiment & annotation$cellTypeId_abInitio == cellId & annotation$stageId == stageId & annotation$strain == strain & annotation$anatId == uberonId & annotation$sex == sex])
-              colnames(infoLib) <- "libraryId"
-              message("Libraries retrieved :", nrow(infoLib))
-              
-              if (nrow(infoLib) == 0){
-                message("No Libraries retrieved!")
-              } else if (nrow(infoLib) >= cellsThreshold){
-                extractInfo <- data.frame(merge(infoLib, annotation, by="libraryId"))
-                information_file <- extractInfo %>% filter(!str_detect(extractInfo$libraryId, "^#"))
-                write.table(extractInfo, file = pass_Libraries, col.names = TRUE , row.names = FALSE ,append = TRUE, quote = FALSE, sep = "\t")
-              } else {
-                extractInfo <- data.frame(merge(infoLib, annotation, by="libraryId"))
-                information_file <- extractInfo %>% filter(!str_detect(extractInfo$libraryId, "^#"))
-                write.table(information_file, file = notpass_Libraries, col.names = TRUE, row.names = FALSE , append = TRUE, quote = FALSE, sep = "\t")
-              }
-            }
-          }
-        }
-      }
-    }
-  }
+# group by species, exp, condition and count number of libraries
+group_by_libs_above_threshold <- annotation %>% group_by(speciesId, experimentId, cellTypeId_abInitio, stageId,
+  strain, anatId, sex) %>% summarize(numberLibs = n()) %>% filter(numberLibs >= as.integer(cellsThreshold))
+
+for(rowNumber in seq(nrow(group_by_libs_above_threshold))) {
+  subset <- annotation[annotation$speciesId %in% group_by_libs_above_threshold$speciesId[rowNumber]
+                       & annotation$experimentId %in% group_by_libs_above_threshold$experimentId[rowNumber]
+                       & annotation$cellTypeId_abInitio %in% group_by_libs_above_threshold$cellTypeId_abInitio[rowNumber]
+                       & annotation$stageId %in% group_by_libs_above_threshold$stageId[rowNumber]
+                       & annotation$strain %in% group_by_libs_above_threshold$strain[rowNumber]
+                       & annotation$anatId %in% group_by_libs_above_threshold$anatId[rowNumber]
+                       & annotation$sex %in% group_by_libs_above_threshold$sex[rowNumber],]
+  passed <- rbind(passed, subset)
 }
+write.table(x = passed, file = pass_Libraries, sep = "\t", col.names = TRUE, row.names = FALSE,
+  quote = FALSE)
+not_passed <- annotation[! annotation$libraryId %in% passed$libraryId,]
+write.table(x = passed, file = notpass_Libraries, sep = "\t", col.names = TRUE, row.names = FALSE,
+            quote = FALSE)
