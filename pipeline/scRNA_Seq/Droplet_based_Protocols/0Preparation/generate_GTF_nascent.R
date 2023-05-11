26d1a28e4a69682c52e20757ddd535905536e297
Julien <julien.wollbrett@gmail.com>
2023-05-11 13:46:54 +0200

    generate nascent gtf and tx2gene for all species with target base libraries

--- pipeline/scRNA_Seq/Droplet_based_Protocols/0Preparation/generate_GTF_nascent.R
+++ pipeline/scRNA_Seq/Droplet_based_Protocols/0Preparation/generate_GTF_nascent.R
@@ -8,15 +8,16 @@
 ## - create a gtf file with each line corresponding to a nascent transcript
 ## - also create a tsv file mapping the new transcript ID to the proper gene ID
 ##
-## This gtf file is used only for single nucléus rnaseq libraries. It allows to map reads to not spliced intron or exon/intron regions
+## This gtf file is used only for single nucleus rnaseq libraries. It allows to map reads to not spliced intron or exon/intron regions
 
 ## Invoking:
-# R CMD BATCH --no-save --no-restore --slave '--args gene_gtf_path = "gene_gtf_path" output_gtf_path = "output_gtf_path"' generate_GTF_nascent.R Rout_path
+# R CMD BATCH --no-save --no-restore --slave '--args gtf_dir = "gtf_dir" metadata_file = "metadata_file"' generate_GTF_nascent.R Rout_path
 
 ## Arguments to provide:
-# "gene_gtf_path"   - full path to gtf file containing gene, transcript, and exon tags
-# "output_gtf_dir" - full path to output folder
-# Rout_path - path to  output .Rout file (optional)
+# "metadata_file"   - full path to the metadata file created by the pipeline
+# "gtf_dir"         - full path to the folder containing GTF files
+
+library(R.utils)
 
 cmd_args = commandArgs(TRUE);
 print(cmd_args)
@@ -27,12 +28,13 @@ if( length(cmd_args) == 0 ){ stop("no arguments provided\n") } else {
 }
 
 ## checking if all necessary arguments were passed....
-command_arg <- c("gene_gtf_path","output_dir")
+command_arg <- c("gtf_dir", "metadata_file")
 for( c_arg in command_arg ){
   if( !exists(c_arg) ){
     stop( paste(c_arg,"command line argument not provided\n") )
   }
 }
+
 ######################## Functions #############################
 
 ## Function for obtaining the part of the annotation field from gtf file
@@ -68,55 +70,96 @@ update_transcript_id <- function(split_annotation){
 
 ###################################################################
 
-## reading in gtf file (gzipped, no need to uncompress)
-cat("Reading GTF file...\n")
-gene_gtf <- as.matrix(read.table(file = gzfile(gene_gtf_path, "r"), sep = "\t", strip.white = TRUE, as.is = TRUE, colClasses = "character", comment.char = '#'))
-## Header lines starting with # are not read
+# read metadata file to retrieve all species for which single cell target base libraries are annotated
+metadata <- read.table(metadata_file, header = TRUE, sep = "\t", comment.char = "")
+speciesNames <- gsub(" ", "_", unique(metadata$scientific_name))
 
-## selecting transcript lines
-gene_gtf_transcript <- gene_gtf[gene_gtf[,3] == "transcript",]
+for (speciesName in speciesNames) {
 
-#transform "transcript" to "exon"
-exon_gtf_nascent <- gene_gtf_transcript
-exon_gtf_nascent[,3] <- "exon"
+  gtf_file <- list.files(path = gtf_dir, pattern = paste0(speciesName, ".*gtf$"), full.names = TRUE)
+  ## there is potentially several gtf files already created.
+  ## All gtf files created from the original Ensembl one have a prefix to specify what they correspond to
+  ## e.g "wo_intergenic" or "nascent". In order to select the proper one we filter on the already known file
+  ## names. It is definitely not bullet proof but the simplest solution to implement
+  gtf_gz_file <- list.files(path = gtf_dir, pattern = paste0(speciesName, ".*gtf.gz$"), full.names = TRUE)
+  gtf_gz_file <- gtf_gz_file[!grepl(pattern = "wo_intergenic.gtf.gz$|nascent.gtf.gz$", x = gtf_gz_file)]
+  ## in case the gtf file is gzipped
+  if (isTRUE(gtf_gz_file)) {
+    gunzip(gtf_gz_file)
+    gtf_file <- list.files(path = gtf_dir, pattern = paste0(speciesName, ".*gtf$"), full.names = TRUE)
+  }
+  gtf_file <- gtf_file[!grepl(pattern = "wo_intergenic.gtf$|nascent.gtf$", x = gtf_file)]
+  
+  ## reading in gtf file (gzipped, no need to uncompress)
+  cat("Reading GTF file...\n")
+  gene_gtf <- as.matrix(read.table(file = gtf_file, sep = "\t", strip.white = TRUE, as.is = TRUE,
+    colClasses = "character", comment.char = '#'))
 
-## splitting the annotation field using single space "; " as a pattern
-split_annotation_list <- strsplit(exon_gtf_nascent[,9], "; ",  fixed  = T)
-#remove exon_id
-field_all <- sapply(split_annotation_list, function(x){ remove_annot_value(x, 'exon_id') })
-field_all <- sapply(field_all, function(x){ add_annot_value(x, 'exon_number 1', 'transcript_id') })
-field_all <- sapply(field_all, function(x){ update_transcript_id(x) })
+  ## selecting transcript lines
+  gene_gtf_transcript <- gene_gtf[gene_gtf[,3] == "transcript",]
 
-exon_gtf_nascent[,9] <- sapply(field_all, function(x){ paste(x,  collapse = "; ") })
+  #transform "transcript" to "exon"
+  exon_gtf_nascent <- gene_gtf_transcript
+  exon_gtf_nascent[,3] <- "exon"
+
+  ## splitting the annotation field using single space "; " as a pattern
+  split_annotation_list <- strsplit(exon_gtf_nascent[,9], "; ",  fixed  = T)
+  #remove exon_id
+  field_all <- sapply(split_annotation_list, function(x){ remove_annot_value(x, 'exon_id') })
+  field_all <- sapply(field_all, function(x){ add_annot_value(x, 'exon_number 1', 'transcript_id') })
+  field_all <- sapply(field_all, function(x){ update_transcript_id(x) })
+
+  exon_gtf_nascent[,9] <- sapply(field_all, function(x){ paste(x,  collapse = "; ") })
 
 
-## getting the vector of the gene IDs (1 for every exon)
-gene_ids <- sapply(split_annotation_list, function(x){ get_annot_value(x, 'gene_id') })
-## getting the vector of the transcript IDs (1 for every exon)
-transcript_ids <- sapply(split_annotation_list, function(x){ get_annot_value(x, 'transcript_id') })
+  ## getting the vector of the gene IDs (1 for every exon)
+  gene_ids <- sapply(split_annotation_list, function(x){ get_annot_value(x, 'gene_id') })
+  ## getting the vector of the transcript IDs (1 for every exon)
+  transcript_ids <- sapply(split_annotation_list, function(x){ get_annot_value(x, 'transcript_id') })
 
-## getting the table with mappings between gene IDs and transcript (for tximport)
-tx2gene_ids <- unique(cbind(transcript_ids, gene_ids), MARGIN = 1)
+  ## getting the table with mappings between gene IDs and transcript (for tximport)
+  tx2gene_nascent_ids <- as.data.frame(unique(cbind(transcript_ids, gene_ids), MARGIN = 1))
+  colnames(tx2gene_nascent_ids) <- c("TXNAME", "GENEID")
+  tx2gene_nascent_ids$TXNAME <- paste0(tx2gene_nascent_ids$TXNAME, "_nascent")
 
-## Output:
-## GTF file with both genic exons and intergenic regions
-nascent_gtf_file <- file.path(output_dir, gsub(pattern = ".gtf", replacement = "_nascent.gtf", x = basename(gene_gtf_path)))
+  ## Output:
+  ## GTF file with both genic exons and intergenic regions
+  nascent_gtf_file <- file.path(gtf_dir, gsub(pattern = ".gtf", replacement = ".nascent.gtf",
+    x = basename(gtf_file)))
 
-message("write file : ", nascent_gtf_file)
-write.table(x = exon_gtf_nascent,
-            file = nascent_gtf_file,
-            sep = "\t",
-            row.names = FALSE,
-            col.names = FALSE,
-            quote = FALSE)
+  message("write file : ", nascent_gtf_file)
+  write.table(x = exon_gtf_nascent,
+              file = nascent_gtf_file,
+              sep = "\t",
+              row.names = FALSE,
+              col.names = FALSE,
+              quote = FALSE)
 
-## Table of mapping between transcript_id and gene_id
-nascent_tx2gene_file <- file.path(output_dir, gsub(pattern = ".gtf", replacement = "_nascent_tx2gene.tsv", x = basename(gene_gtf_path)))
+  ## Table of mapping between transcript_id and gene_id
+  tx2gene_file <- file.path(gtf_dir, gsub(pattern = ".gtf", replacement = ".tx2gene",
+    x = basename(gtf_file)))
+  if (!file.exists(tx2gene_file)) {
+    if(file.exists(paste0(tx2gene_file, ".xz"))) {
+      system(sprintf("unxz %s", paste0(tx2gene_file, ".xz")))
+    } else {
+      stop("transcript to gene file [", tx2gene_file, "] not found.")
+    }
+  }
+  tx2gene_ids <- read.table(file = tx2gene_file, sep = "\t", header = TRUE)
+  tx2gene_single_nucleus_ids <- rbind(tx2gene_ids, tx2gene_nascent_ids)
 
-message("write file : ", nascent_tx2gene_file)
-write.table(x = tx2gene_ids,
-            file = nascent_tx2gene_file,
-            sep = "\t",
-            row.names = FALSE,
-            col.names = c("TXNAME", "GENEID"),
-            quote = FALSE)
+  single_nucleus_tx2gene_file <- file.path(gtf_dir, gsub(pattern = ".gtf",
+    replacement = ".single_nucleus.tx2gene", x = basename(gtf_file)))
+
+  message("write file : ", single_nucleus_tx2gene_file)
+  write.table(x = tx2gene_single_nucleus_ids,
+              file = single_nucleus_tx2gene_file,
+              sep = "\t",
+              row.names = FALSE,
+              col.names = TRUE,
+              quote = FALSE)
+
+  # now that all files are created for the species we can gzip the original gtf file
+  gzip(gtf_file)
+}
+
