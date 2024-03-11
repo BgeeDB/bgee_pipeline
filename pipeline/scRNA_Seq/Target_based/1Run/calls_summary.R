8189c2dabee6aba90ecd94b488fdd6c6e41b5900
jwollbrett <julien.wollbrett@sib.swiss>
2024-03-11 15:49:15 +0100

    generate plots summarizing droplet based calls

--- pipeline/scRNA_Seq/Target_based/1Run/calls_summary.R
+++ pipeline/scRNA_Seq/Target_based/1Run/calls_summary.R
@@ -28,15 +28,33 @@ for (c_arg in command_arg) {
 }
 if (! file.exists(metadata_file)) {
   stop("metadata file ",metadata_file, " does not exist.")
+}
 metadata <- read.table(file = metadata_file, header = TRUE, sep = "\t", quote = FALSE)
 summaryData <- c()
 for (libraryId in sort(unique(metadata$library_id))) {
   libraryCallsFile <- file.path(calls_dir, libraryId, paste0(libraryId, "stats.tsv"))
   if (! file.exists(libraryCallsFile)) {
     stop("an error happened in the generation of calls for library ", libraryId, ". The calls summary file for that library has not been generated.",
-    " Please solve the issue or remove this library from the metadata file and rerun the rule.")
+         " Please solve the issue or remove this library from the metadata file and rerun the rule.")
   }
   librarySummary <- read.table(file = libraryCallsFile, header = TRUE, sep = "\t", quote = FALSE)
   summaryData <- rbind(summaryData, librarySummary)
 }
 write.table(x = summaryData, file = summary_calls_file, row.names = FALSE, col.names = TRUE, quote = FALSE, sep = "\t")
+  
+## final plot per species
+pdf(file.path(output_folder, paste0("All_libraries_stats_information_10X.pdf")), width=16, height=6) 
+g1 <- ggplot(summaryData, aes(x=organism, y=as.numeric(Proportion_genic_present))) + 
+  geom_boxplot()+ylim(0,100)+xlab(" ")+ylab("% Genic Present")
+g2 <- ggplot(summaryData, aes(x=organism, y=as.numeric(Proportion_coding_present))) + 
+  geom_boxplot(notch=TRUE)+ylim(0,100)+xlab(" ")+ylab("% Protein Coding Present")
+g3 <- ggplot(summaryData, aes(x=organism, y=as.numeric(Proportion_intergenic_present))) + 
+  geom_boxplot(notch=TRUE)+ylim(0,100)+xlab(" ")+ylab("% Intergenic Present")
+g4 <- ggplot(summaryData, aes(x=organism, y=as.numeric(meanRefIntergenic))) + 
+  geom_boxplot(notch=TRUE)+ylim(0,1)+xlab(" ")+ylab("Mean Reference Intergenic")
+g5 <- ggplot(summaryData, aes(x=organism, y=as.numeric(sdRefIntergenic))) + 
+  geom_boxplot(notch=TRUE)+ylim(0,10)+xlab(" ")+ylab("SD Reference Intergenic")
+g6 <- ggplot(summaryData, aes(x=organism, y=as.numeric(pValue_cutoff))) + 
+  geom_boxplot(notch=TRUE)+ylim(0,1)+xlab(" ")+ylab("pValue_cutoff")
+grid.arrange(g1,g2,g3,g4,g5,g6, nrow = 2, ncol = 3)
+dev.off()
