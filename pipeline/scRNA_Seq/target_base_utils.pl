ab0377cb5fbd0f38dabd6a9c5e8ba6d4777b67e2
jwollbrett <julien.wollbrett@sib.swiss>
2024-03-20 16:34:04 +0100

    improve organisation of representation of metadata as a hash

--- pipeline/scRNA_Seq/target_base_utils.pl
+++ pipeline/scRNA_Seq/target_base_utils.pl
@@ -362,7 +362,7 @@ sub get_processed_libraries_info {
             $downloadSource                 = $tmp[11];
         }
 
-        if (!defined $libInfos{$experimentId}->{$libraryId}->{$runId}) {
+        if (!defined $libInfos{$experimentId}->{$libraryId}->{'runIds'}->{$runId}) {
             # Perform format checks
             # do not check format for genotype and whiteList as they can be empty
             my $discarded = 0;
