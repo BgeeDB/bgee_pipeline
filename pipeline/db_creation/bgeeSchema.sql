6cfa403bf162a178bd323c9e0d20dfb209a7cffd
Julien Wollbrett <julien.wollbrett@unil.ch>
2025-04-15 13:20:23 +0200

    add expressionSummary column to the database schema

--- pipeline/db_creation/bgeeSchema.sql
+++ pipeline/db_creation/bgeeSchema.sql
@@ -492,7 +492,8 @@ create table gene (
 -- (for instance, bonobo; we use chimp genome)
     ensemblGene boolean not null default 1 COMMENT 'Is the gene in Ensembl (default) (= 1), if not (= 0)',
     seqRegionName varchar(255) not null default '' COMMENT 'Chromosomal or assembly name where this gene is',
-    geneMappedToGeneIdCount tinyint unsigned not null default 1 COMMENT 'number of genes in the Bgee database with the same Ensembl gene ID. In Bgee, for some species with no genome available, we use the genome of a closely-related species, such as chimpanzee genome for analyzing bonobo data. For this reason, a same Ensembl gene ID can be mapped to several species in Bgee. The value returned here is equal to 1 when the Ensembl gene ID is uniquely used in the Bgee database.'
+    geneMappedToGeneIdCount tinyint unsigned not null default 1 COMMENT 'number of genes in the Bgee database with the same Ensembl gene ID. In Bgee, for some species with no genome available, we use the genome of a closely-related species, such as chimpanzee genome for analyzing bonobo data. For this reason, a same Ensembl gene ID can be mapped to several species in Bgee. The value returned here is equal to 1 when the Ensembl gene ID is uniquely used in the Bgee database.',
+    expressionSummary varchar(255) not null default '' COMMENT 'Sentence generated from propagated expression calls that summarizes the expression for the anatomical entity and the celltype'
 ) engine = innodb;
 
 create table geneNameSynonym (
