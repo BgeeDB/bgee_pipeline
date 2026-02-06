2d50677f0a216ddb1de46b6fe2ea09c4da049261
Julien Wollbrett <julien.wollbrett@unil.ch>
2026-02-06 15:40:19 +0100

    better management of barcode multiplet

--- pipeline/scRNA_Seq/Target_based/0Preparation/cleaning_barcodes.R
+++ pipeline/scRNA_Seq/Target_based/0Preparation/cleaning_barcodes.R
@@ -52,17 +52,39 @@ for (barcode_file_path in barcodes_files_path) {
   barcode_file_name <- basename(barcode_file_path)
   unique_barcodes_list <- list()
   duplicated_barcodes_list <- list()
+  # Initialize statistics tracking
+  stats_same_annotation <- list()
+  stats_different_annotation <- list()
+  
   for (library_id in unique(barcodes$library)) {
     cluster_id <- 1
     select_barcodes <- dplyr::filter(barcodes, barcodes$library == library_id)
     
+    # Count duplicates with same annotation (cellTypeId and cell_type)
+    duplicates_same_annotation <- select_barcodes %>%
+      group_by(barcode) %>%
+      filter(n() > 1 & n_distinct(cellTypeId) == 1 & n_distinct(cell_type) == 1) %>%
+      ungroup()
+    
+    num_barcodes_same_annotation <- n_distinct(duplicates_same_annotation$barcode)
+    stats_same_annotation[[library_id]] <- num_barcodes_same_annotation
+    
+    # Get the extra occurrences of duplicates with same annotation (all but the first one)
+    # These will be moved to the Duplicated file
+    duplicates_same_annotation_extra <- select_barcodes %>%
+      group_by(barcode) %>%
+      filter(n() > 1 & n_distinct(cellTypeId) == 1 & n_distinct(cell_type) == 1) %>%
+      slice(-1) %>%  # Remove first occurrence, keep all others
+      ungroup()
+    
     #select only unique barcodes/library. This check should be done at annotation level before running the pipeline
     unique_barcodes <- select_barcodes %>%
       group_by(barcode) %>% 
-      # filter barcodes that are unique or that are duplicated but with the same cellTypeId.
+      # keep barcodes that are unique or that are duplicated but with the same cellTypeId and cell_type.
       # Keep only one occurance of deplicated barcodes with same cellTypeId
-      filter(n() == 1 | n_distinct(cell_type) == 1)
-    
+      filter(n_distinct(cellTypeId) == 1 & n_distinct(cell_type) == 1) %>%
+      slice(1)
+
     uniq_celltype_freetext <- unique(unique_barcodes$cell_type)
     # As the cell_type column is used to detect clusters of barcodes, we can not have cell_type that is NA
     contains_celltype_freeText <- TRUE
@@ -75,11 +97,14 @@ for (barcode_file_path in barcodes_files_path) {
     uniq_cluster_id <- unique(unique_barcodes$cluster)
     if (is.null(uniq_cluster_id) || all(is.na(uniq_cluster_id)) || (length(uniq_cluster_id) == 1 && uniq_cluster_id == "")) {
       unique_barcodes$cluster <- ""
-      message("No cluster ID provided for file ", barcode_file_name, " and library ", library_id)
       contains_cluster <- FALSE
     }
     if (contains_cluster && !contains_celltype_freeText) {
-      if (length(uniq_cluster_id) != length(unique_barcodes$cellTypeId[unique_barcodes$cluster %in% uniq_cluster_id])) {
+      # Check if each cluster ID maps to exactly one cellTypeId (1-to-1 relationship)
+      num_unique_clusters <- length(uniq_cluster_id)
+      num_cluster_celltype_combinations <- nrow(unique(unique_barcodes[, c("cluster", "cellTypeId")]))
+      
+      if (num_unique_clusters != num_cluster_celltype_combinations) {
         message("cluster IDs are not unique for a combination of library and celltypeId for file ", barcode_file_name,
           ". Removes cluster IDs from that file in order not to store it as author celltype info in the Bgee database")
         unique_barcodes$cluster <- ""
@@ -99,7 +124,6 @@ for (barcode_file_path in barcodes_files_path) {
     # then they are used to generate the internal cluster IDs
     if (contains_celltype_freeText) {
       for (celltype_author_annotation in unique(unique_barcodes$cell_type)) {
-        message("cell type for library ", library_id, " is ", celltype_author_annotation)
         unique_barcodes$internal_cluster_id[unique_barcodes$cell_type == celltype_author_annotation] <- cluster_id
         cluster_id <- cluster_id + 1
       }
@@ -113,9 +137,27 @@ for (barcode_file_path in barcodes_files_path) {
       }
     }
 
-    barcodes_duplicates <- select_barcodes %>% 
+    # Get duplicates with different annotation - ALL occurrences go to duplicated file
+    barcodes_duplicates_different <- select_barcodes %>% 
       group_by(barcode) %>% 
-      filter(n() != 1 & n_distinct(cell_type) > 1)
+      filter(n() > 1 & (n_distinct(cellTypeId) > 1 | n_distinct(cell_type) > 1))
+    
+    # Combine both types of duplicates for the Duplicated file
+    barcodes_duplicates <- bind_rows(duplicates_same_annotation_extra, barcodes_duplicates_different)
+    
+    # Count duplicates with different annotation
+    num_barcodes_different_annotation <- n_distinct(barcodes_duplicates_different$barcode)
+    stats_different_annotation[[library_id]] <- num_barcodes_different_annotation
+    
+    # Report statistics for this library
+    if (num_barcodes_same_annotation > 0) {
+      message("Library ", library_id, ": ", num_barcodes_same_annotation, 
+              " barcodes were present more than once with same cellTypeId and cell_type (keeping one occurrence, others in Duplicated file)")
+    }
+    if (num_barcodes_different_annotation > 0) {
+      message("Library ", library_id, ": ", num_barcodes_different_annotation, 
+              " barcodes were present more than once with different annotations (all moved to Duplicated file)")
+    }
 
     ## Append results to lists
     unique_barcodes_list[[library_id]] <- unique_barcodes
@@ -127,6 +169,16 @@ for (barcode_file_path in barcodes_files_path) {
   all_unique_barcodes <- bind_rows(unique_barcodes_list)
   all_duplicated_barcodes <- bind_rows(duplicated_barcodes_list)
   
+  # Summary statistics for the entire file
+  total_same_annotation <- sum(unlist(stats_same_annotation))
+  total_different_annotation <- sum(unlist(stats_different_annotation))
+  if (total_same_annotation > 0 || total_different_annotation > 0) {
+    message("====== SUMMARY for ", barcode_file_name, " ======")
+    message("Total barcodes with duplicates (same annotation): ", total_same_annotation)
+    message("Total barcodes with duplicates (different annotation): ", total_different_annotation)
+    message("================================================")
+  }
+  
   tryCatch(
     write.table(all_unique_barcodes, file = file.path(output, barcode_file_name), sep = "\t", row.names = FALSE, quote = TRUE),
     error = function(e) {
@@ -135,9 +187,6 @@ for (barcode_file_path in barcodes_files_path) {
   )
 
   if (nrow(all_duplicated_barcodes) > 0){
-    message("Exists barcode duplicates in 1 or more library of ", barcode_file_name)
     write.table(all_duplicated_barcodes, file = file.path(output, paste0("DuplicateBarcodes_", barcode_file_name)), sep = "\t", row.names = FALSE, quote = FALSE)
-  } else {
-    message("No barcode duplicates in any libraryID of the experiment: ", barcode_file_name)
   }
 }
