3f0b81b10799d01f29f940f28dc253c12017d59c
Julien Wollbrett <julien.wollbrett@unil.ch>
2026-06-02 10:45:07 +0200

    manually updated the file listing datasources per species/datatypes

--- pipeline/db_creation/insert_data_sources_to_species.sql
+++ pipeline/db_creation/insert_data_sources_to_species.sql
@@ -36,42 +36,6 @@ INSERT INTO dataSourceToSpecies (dataSourceId, speciesId, dataType, infoType) VA
 (15, 8364, 'in situ', 'annotation');
 
 
--- AFFYMETRIX --------------------------------
-
--- ArrayExpress source for Affymetrix data in drosophila, human, zebrafish, mouse
-INSERT INTO dataSourceToSpecies (dataSourceId, speciesId, dataType, infoType) VALUES
-(11, 7227,  'affymetrix', 'data'),
-(11, 9606,  'affymetrix', 'data'),
-(11, 7955,  'affymetrix', 'data'),
-(11, 10090, 'affymetrix', 'data');
--- GEO, source for Affymetrix data in c. elegans, drosophila, human, zebrafish, mouse
-INSERT INTO dataSourceToSpecies (dataSourceId, speciesId, dataType, infoType) VALUES
-(17, 6239,  'affymetrix', 'data'),
-(17, 7227,  'affymetrix', 'data'),
-(17, 7955,  'affymetrix', 'data'),
-(17, 9544,  'affymetrix', 'data'),
-(17, 9606,  'affymetrix', 'data'),
-(17, 10090, 'affymetrix', 'data'),
-(17, 10116, 'affymetrix', 'data');
-
-
--- EST ---------------------------------------
-
--- Unigene source for EST data in droso, zebrafish, xenopus, human, mouse
-INSERT INTO dataSourceToSpecies (dataSourceId, speciesId, dataType, infoType) VALUES
-(12, 7227,  'est', 'data'),
-(12, 7955,  'est', 'data'),
-(12, 8364,  'est', 'data'),
-(12, 9606,  'est', 'data'),
-(12, 10090, 'est', 'data');
--- smiRNAdb source for EST data in zebrafish human mouse
--- we hide it because it's outdated
--- INSERT INTO dataSourceToSpecies (dataSourceId, speciesId, dataType, infoType) VALUES
--- (13, 7955,  'est', 'data'),
--- (13, 9606,  'est', 'data'),
--- (13, 10090, 'est', 'data');
-
-
 -- RNA-SEQ -----------------------------------
 
 -- SRA source of RNA-Seq data in lots of species :p
@@ -189,62 +153,27 @@ INSERT INTO dataSourceToSpecies (dataSourceId, speciesId, dataType, infoType) VA
 INSERT INTO dataSourceToSpecies (dataSourceId, speciesId, dataType, infoType) VALUES
 (23, 6239, 'in situ',    'data'),
 (23, 6239, 'in situ',    'annotation'),
-(23, 6239, 'affymetrix', 'annotation'),
 (23, 6239, 'rna-seq',    'annotation');
 
 
 -- Bgee source of annotation for EST, Affymetrix and RNA-Seq data in all species
 INSERT INTO dataSourceToSpecies (dataSourceId, speciesId, dataType, infoType) VALUES
-(25, 6239,   'affymetrix', 'annotation'),
-(25, 6239,   'est',        'annotation'),
 (25, 6239,   'rna-seq',    'annotation'),
-(25, 7227,   'affymetrix', 'annotation'),
-(25, 7227,   'est',        'annotation'),
 (25, 7227,   'rna-seq',    'annotation'),
-(25, 7955,   'affymetrix', 'annotation'),
-(25, 7955,   'est',        'annotation'),
 (25, 7955,   'rna-seq',    'annotation'),
-(25, 8364,   'affymetrix', 'annotation'),
-(25, 8364,   'est',        'annotation'),
 (25, 8364,   'rna-seq',    'annotation'),
-(25, 9031,   'affymetrix', 'annotation'),
-(25, 9031,   'est',        'annotation'),
 (25, 9031,   'rna-seq',    'annotation'),
-(25, 9258,   'affymetrix', 'annotation'),
-(25, 9258,   'est',        'annotation'),
 (25, 9258,   'rna-seq',    'annotation'),
-(25, 9544,   'affymetrix', 'annotation'),
-(25, 9544,   'est',        'annotation'),
 (25, 9544,   'rna-seq',    'annotation'),
-(25, 9593,   'affymetrix', 'annotation'),
-(25, 9593,   'est',        'annotation'),
 (25, 9593,   'rna-seq',    'annotation'),
-(25, 9597,   'affymetrix', 'annotation'),
-(25, 9597,   'est',        'annotation'),
 (25, 9597,   'rna-seq',    'annotation'),
-(25, 9598,   'affymetrix', 'annotation'),
-(25, 9598,   'est',        'annotation'),
 (25, 9598,   'rna-seq',    'annotation'),
-(25, 9606,   'affymetrix', 'annotation'),
-(25, 9606,   'est',        'annotation'),
 (25, 9606,   'rna-seq',    'annotation'),
-(25, 9823,   'affymetrix', 'annotation'),
-(25, 9823,   'est',        'annotation'),
 (25, 9823,   'rna-seq',    'annotation'),
-(25, 9913,   'affymetrix', 'annotation'),
-(25, 9913,   'est',        'annotation'),
 (25, 9913,   'rna-seq',    'annotation'),
-(25, 10090,  'affymetrix', 'annotation'),
-(25, 10090,  'est',        'annotation'),
 (25, 10090,  'rna-seq',    'annotation'),
-(25, 10116,  'affymetrix', 'annotation'),
-(25, 10116,  'est',        'annotation'),
 (25, 10116,  'rna-seq',    'annotation'),
-(25, 13616,  'affymetrix', 'annotation'),
-(25, 13616,  'est',        'annotation'),
 (25, 13616,  'rna-seq',    'annotation'),
-(25, 28377,  'affymetrix', 'annotation'),
-(25, 28377,  'est',        'annotation'),
 (25, 28377,  'rna-seq',    'annotation'),
 (25, 7237,   'rna-seq',    'annotation'),
 (25, 7240,   'rna-seq',    'annotation'),
@@ -284,15 +213,43 @@ INSERT INTO dataSourceToSpecies (dataSourceId, speciesId, dataType, infoType) VA
 
 -- Bgee source of annotation for single cell data
 INSERT INTO dataSourceToSpecies (dataSourceId, speciesId, dataType, infoType) VALUES
-(25, 9606,   'single-cell RNA-Seq', 'annotation'),
-(25, 10090,  'single-cell RNA-Seq', 'annotation'),
-(25, 7227,  'single-cell RNA-Seq', 'annotation');
+(25, 7227,  'single-cell RNA-Seq', 'annotation'),
+(25, 7955,  'single-cell RNA-Seq', 'annotation'),
+(25, 9031,  'single-cell RNA-Seq', 'annotation'),
+(25, 9258,  'single-cell RNA-Seq', 'annotation'),
+(25, 9483,  'single-cell RNA-Seq', 'annotation'),
+(25, 9541,  'single-cell RNA-Seq', 'annotation'),
+(25, 9544,  'single-cell RNA-Seq', 'annotation'),
+(25, 9545,  'single-cell RNA-Seq', 'annotation'),
+(25, 9593,  'single-cell RNA-Seq', 'annotation'),
+(25, 9598,  'single-cell RNA-Seq', 'annotation'),
+(25, 9606,  'single-cell RNA-Seq', 'annotation'),
+(25, 9823,  'single-cell RNA-Seq', 'annotation'),
+(25, 9986,  'single-cell RNA-Seq', 'annotation'),
+(25, 10090, 'single-cell RNA-Seq', 'annotation'),
+(25, 10116, 'single-cell RNA-Seq', 'annotation'),
+(25, 10181, 'single-cell RNA-Seq', 'annotation'),
+(25, 13616, 'single-cell RNA-Seq', 'annotation');
 
 -- source of data for single cell data
 INSERT INTO dataSourceToSpecies (dataSourceId, speciesId, dataType, infoType) VALUES
-(102, 9606,   'single-cell RNA-Seq', 'data'),
-(102, 10090,  'single-cell RNA-Seq', 'data'),
-(102, 7227,  'single-cell RNA-Seq', 'data');
+(102, 7227,  'single-cell RNA-Seq', 'data'),
+(102, 7955,  'single-cell RNA-Seq', 'data'),
+(102, 9031,  'single-cell RNA-Seq', 'data'),
+(102, 9258,  'single-cell RNA-Seq', 'data'),
+(102, 9483,  'single-cell RNA-Seq', 'data'),
+(102, 9541,  'single-cell RNA-Seq', 'data'),
+(102, 9544,  'single-cell RNA-Seq', 'data'),
+(102, 9545,  'single-cell RNA-Seq', 'data'),
+(102, 9593,  'single-cell RNA-Seq', 'data'),
+(102, 9598,  'single-cell RNA-Seq', 'data'),
+(102, 9606,  'single-cell RNA-Seq', 'data'),
+(102, 9823,  'single-cell RNA-Seq', 'data'),
+(102, 9986,  'single-cell RNA-Seq', 'data'),
+(102, 10090, 'single-cell RNA-Seq', 'data'),
+(102, 10116, 'single-cell RNA-Seq', 'data'),
+(102, 10181, 'single-cell RNA-Seq', 'data'),
+(102, 13616, 'single-cell RNA-Seq', 'data');
 
 -- GTEx data source
 INSERT INTO dataSourceToSpecies (dataSourceId, speciesId, dataType, infoType) VALUES
